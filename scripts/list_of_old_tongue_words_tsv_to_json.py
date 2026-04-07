464baa2d5f667e466e188d83f6efa2ea36098aa7
Andrew F <ncsuandrew12@gmail.com>
2026-04-07 18:56:17 +0000

    Added Companion's Old Tongue Dictionary. Added code for converting Old Tongue words spreadsheets to JSON files.

---
+++ scripts/list_of_old_tongue_words_tsv_to_json.py
@@ -0,0 +1,113 @@
+import argparse
+import json
+import math
+import os
+import sys
+import re
+import time
+from command import Command, Progresser, Verbosity, run_command
+from log_utils import logger as log
+from pathlib import Path
+from utils import Ticker
+
+class ConvertOtDictToWiki(Command):
+
+    def __init__(self, args):
+        Command.__init__(self, args)
+
+    def create_arg_parser(self):
+        parser = argparse.ArgumentParser(
+            description=""
+        )
+        parser.add_argument(
+            "--single-input-file",
+            action="store",
+            default="./OTSingle.txt")
+        parser.add_argument(
+            "--compound-input-file",
+            action="store",
+            default="./OTCompound.txt")
+        parser.add_argument(
+            "--in-json",
+            action="store",
+            default="../wotwiki/source material/companion-old-tongue.json",
+            help="Path to the input JSON file.")
+        parser.add_argument(
+            "--out-json",
+            action="store",
+            default="./old-tongue-dict.json")
+        return parser
+
+    def run_command(self):
+        otd = json.load(open(self.parsed_args.in_json, "r"))
+        with open(self.parsed_args.single_input_file, "r", encoding="cp1252") as f:
+            for line in f:
+                line = line.strip()
+                log.debug(f"Processing line: {line}")
+                if line == "" or line.startswith("#"):
+                    continue
+                word, defn, part = line.split("\t")
+                parts = self.get_parts(part, line)
+                if not word in otd:
+                    otd[word] = { "word": word, "definition": { "wotwiki_custom": defn }, "parts": parts }
+                elif "entries" not in otd[word]:
+                    otd[word] = { "word": word, "entries": [ otd[word], { "definition": { "wotwiki_custom": defn }, "parts": parts } ] }
+                    self.fixup_entry(otd[word])
+                else:
+                    otd[word]["entries"].append({ "definition": { "wotwiki_custom": defn }, "parts": parts })
+        with open(self.parsed_args.compound_input_file, "r", encoding="cp1252") as f:
+            for line_str in f:
+                line_str = line_str.strip()
+                log.debug(f"Processing line: {line_str}")
+                if line_str == "" or line_str.startswith("#"):
+                    continue
+                try:
+                    word, common, literal, part, notes, _ = line_str.split("\t")
+                except Exception as e:
+                    raise Exception(f"Error processing line_str: {line_str}: {e}")
+                parts = self.get_parts(part, line)
+                parts.append({ "type": "compound" })
+                if not word in otd:
+                    otd[word] = { "word": word, "definition": { "wotwiki_common": common, "wotwiki_literal": literal }, "parts": parts, "notes": notes }
+                elif "entries" not in otd[word]:
+                    otd[word] = { "word": word, "entries": [ otd[word], { "definition": { "wotwiki_common": common, "wotwiki_literal": literal }, "parts": parts, "notes": notes } ] }
+                    self.fixup_entry(otd[word])
+                else:
+                    otd[word]["entries"].append({ "definition": { "wotwiki_common": common, "wotwiki_literal": literal }, "parts": parts, "notes": notes })
+        with open(self.parsed_args.out_json, "w") as f:
+            json.dump(otd, f, indent=2)
+        return 0
+
+    def get_parts(self, part, line):
+        parts = []
+        for _, part in enumerate(re.sub(r";\s+", ";", part).split(";")):
+            modifiers = []
+            match = re.match(r"^(.+)\s+\(([^)]+)\)$", part)
+            if match:
+                part = match.group(1).strip()
+                m = match.group(2).strip()
+                if m not in [ "auxiliary", "for nouns", "for adjectives", "possessive" ]:
+                    raise Exception(f"Error: unrecognized part of speech modifier: {m}: {line}")
+                modifiers.append(m)
+            if part not in [ "adjective", "interjection", "adverb","combination","conjunction","noun",
+                            "past participle","possessive pronoun","prefix","preposition","pronoun",
+                            "relative pronoun","verb","suffix", "past participle" ]:
+                if part == "nou":
+                    part = "noun"
+                elif part == "past partciple":
+                    part = "past participle"
+                else:
+                    raise Exception(f"Error: unrecognized part of speech: {part}: {line}")
+            p = { "type": part }
+            if len(modifiers) > 0:
+                p["modifiers"] = modifiers
+            parts.append(p)
+        return parts
+
+    def fixup_entry(self, entry):
+        del entry["entries"][0]["word"]
+        if "italicize" in entry["entries"][0]:
+            entry["italicize"] = entry["entries"][0]["italicize"]
+            del entry["entries"][0]["italicize"]
+
+exit(run_command(ConvertOtDictToWiki(sys.argv)))
--- scripts/old_tongue_dict_to_wiki.py
+++ scripts/old_tongue_dict_to_wiki.py
@@ -1,14 +1,10 @@
 import argparse
 import json
-import math
-import os
 import sys
 import re
-import time
-from command import Command, Progresser, Verbosity, run_command
+from command import Command, Progresser,  run_command
 from log_utils import logger as log
 from pathlib import Path
-from utils import Ticker
 
 class ConvertOtDictToWiki(Command):
 
@@ -22,71 +18,183 @@ class ConvertOtDictToWiki(Command):
         parser.add_argument(
             "--input-file",
             action="store",
-            default="./source material/companion-old-tongue.md",
+            default="../wotwiki/source material/companion-old-tongue.md",
             help="Path to the input Markdown file containing the Old Tongue dictionary.")
         parser.add_argument(
             "--out-json",
             action="store",
-            default="./source material/companion-old-tongue.json",
+            default="../wotwiki/source material/companion-old-tongue.json",
             help="Path to the output JSON file.")
         parser.add_argument(
-            "--out-json-force",
+            "--overwrite-out-json",
             action="store_true",
             help="Overwrite the output JSON file.")
         return parser
 
-    def finish_entry(self, entries, entry):
-        log.debug(f"Finishing entry: {entry}")
-        new_entries = []
-        r = r"(.*)\[([^\]]+)-*\]-*(\([^\)]+\)){0,1}\s*(\S.*)"
-        match = re.match(r, entry["definition"])
-        while match:
-            log.debug("Contains another entry")
-            entry["definition"] = re.sub(r"\s*;*\s*$", "", re.sub(r, r"\1", entry["definition"], count=1))
-            ne = { "word": match.group(2), "definition": match.group(4) }
-            if len(match.group(3) or "") > 2:
-                ne["part"] = match.group(3)[1:-1]
-            elif "part" in entry:
-                ne["part"] = entry["part"]
-            elif "ref" in entry:
-                ne["ref"] = entry["ref"]
-            entries[entry['word']] = entry
-            new_entries.append(ne)
-            match = re.match(r, entry["definition"])
-        for entry in new_entries:
-            entries[entry['word']] = entry
-            self.finish_entry(entries, entry)
+    def add_entry(self, entries, sub_entry):
+        if sub_entry["word"] in entries:
+            entries[sub_entry["word"]].append(sub_entry)
+        else:
+            entries[sub_entry["word"]] = [sub_entry]
+
+    def finish_sub_entry(self, entries, sub_entry):
+        try:
+            log.debug(f"Finishing sub_entry: {sub_entry}")
+            new_entries = []
+            r = r"(.*)(?<!e\.g\., )(?<!e\.g\., in )\[([^\/\]]+)-*\](?>! = )-*(\([^\)]+\)){0,1}\s*(\S.*)"
+            match = re.match(r, sub_entry["definition"]["companion_epub_md"])
+            while match:
+                word = match.group(2)
+                if word in entries:
+                    match = None
+                    continue
+                log.debug("Contains another sub_entry")
+                sub_entry["definition"]["companion_epub_md"] = re.sub(r"\s*;*\s*$", "", re.sub(r, r"\1", sub_entry["definition"]["companion_epub_md"], count=1))
+                ne = { "word": word, "definition": { "companion_epub_md": match.group(4) } }
+                if len(match.group(3) or "") > 2:
+                    ne["parts"] = match.group(3)[1:-1]
+                elif "parts" in sub_entry:
+                    ne["parts"] = sub_entry["parts"]
+                elif "ref" in sub_entry:
+                    ne["ref"] = sub_entry["ref"]
+                self.add_entry(entries, sub_entry)
+                new_entries.append(ne)
+                match = re.match(r, sub_entry["definition"]["companion_epub_md"])
+            for sub_entry in new_entries:
+                self.add_entry(entries, sub_entry)
+                self.finish_sub_entry(entries, sub_entry)
+            sub_entry["definition"]["companion_epub_md"] = re.sub(r"^=\s*, ", "", sub_entry["definition"]["companion_epub_md"])
+            sub_entry["definition"]["companion"] = re.sub(r"[\[\]]", "", sub_entry["definition"]["companion_epub_md"])
+            sub_entry["definition"]["wotwiki"] = re.sub(r"\[", r"<i>[[",
+                re.sub(r"\]", r"]]</i>",
+                       re.sub(r"\[([^/\]]+)\/([^\]]+)\]", r"[\1]/[\2]", sub_entry["definition"]["companion_epub_md"])))
+            if "parts" in sub_entry:
+                sub_entry["parts_companion"] = sub_entry["parts"]
+                if type(sub_entry["parts"]) == str:
+                    if sub_entry["parts"] == "complex word form":
+                        sub_entry["parts"] = [ { "type": "complex word form" } ]
+                    else:
+                        sub_entry["parts"] = re.sub(r"[\s&,]+", ";",
+                            re.sub("generally ", "generally_",
+                            re.sub(" aux", "_aux",
+                            re.sub(" neg", "_neg",
+                            re.sub("past part", "past_part",
+                            re.sub("rel pron", "rel_pron",
+                            re.sub("poss pron", "poss_pron",
+                            re.sub(r"[\.]", "", sub_entry["parts"])))))))).split(";")
+                        for i, part in enumerate(sub_entry["parts"]):
+                            modifiers = []
+                            if part.startswith("generally_"):
+                                part = part[len("generally_"):]
+                                modifiers.append("generally")
+                            if part.endswith("_aux"):
+                                part = part[:-len("_aux")]
+                                modifiers.append("auxilliary")
+                            if part.endswith("_neg"):
+                                part = part[:-len("_neg")]
+                                modifiers.append("negative")
+                            if part == "adj":
+                                sub_entry["parts"][i] = "adjective"
+                            elif part == "adv":
+                                sub_entry["parts"][i] = "adverb"
+                            elif part == "comb":
+                                sub_entry["parts"][i] = "combination"
+                            elif part == "conj":
+                                sub_entry["parts"][i] = "conjunction"
+                            elif part == "interjection":
+                                pass
+                            elif part == "n":
+                                sub_entry["parts"][i] = "noun"
+                            elif part == "past_part":
+                                sub_entry["parts"][i] = "past participle"
+                            elif part == "poss_pron":
+                                sub_entry["parts"][i] = "possessive pronoun"
+                            elif part == "prefix":
+                                sub_entry["parts"][i] = "prefix"
+                            elif part == "prep":
+                                sub_entry["parts"][i] = "preposition"
+                            elif part == "pron":
+                                sub_entry["parts"][i] = "pronoun"
+                            elif part == "rel_pron":
+                                sub_entry["parts"][i] = "relative pronoun"
+                            elif part == "v":
+                                sub_entry["parts"][i] = "verb"
+                            elif part == "suffix":
+                                sub_entry["parts"][i] = "suffix"
+                            else:
+                                raise Exception(f"Error: unrecognized part of speech: {part}")
+                            sub_entry["parts"][i] = { "type": sub_entry["parts"][i] }
+                            if len(modifiers) > 0:
+                                sub_entry["parts"][i]["modifiers"] = modifiers
+        except Exception as e:
+            log.error(f"Error processing sub_entry {sub_entry['word']}: {e}")
+            raise Exception(f"Error processing sub_entry {sub_entry['word']}: {sub_entry} {e}")
 
     def run_command(self):
-        if (not self.parsed_args.out_json_force and Path(self.parsed_args.out_json).exists()):
+        if (not self.parsed_args.overwrite_out_json and Path(self.parsed_args.out_json).exists()):
             raise Exception(f"{self.parsed_args.out_json} already exists, refusing to overwrite.")
         log.info(f"Loading input file {self.parsed_args.input_file}")
-        self.print_n("Processing words", end="", flush=True)
+        self.print_n("Processing word lines", end="", flush=True)
         entries = {}
-        entry = None
-        progresser = Progresser(self)
+        sub_entry = None
+        progresser = Progresser(self, period=1)
         with open(self.parsed_args.input_file, "r") as input_f:
             for line in input_f:
                 line = line.strip()
                 log.debug(f"line: {line}")
                 progresser.tick()
                 if line == "":
-                    self.finish_entry(entries, entry)
-                    entry = None
+                    # if sub_entry:
+                    #     self.add_entry(entries, sub_entry)
+                    sub_entry = None
+                elif sub_entry == None:
+                    match = re.match(r'^\[{0,1}([^\]]+)\]{0,1}---\]{0,1}(\([^\)]+\)){0,1}\s*(\S.*)$', line)
+                    if not match:
+                        raise Exception(f"Error: unrecognized entry format: {line}")
+                    sub_entry = { "word": match.group(1), "definition": { "companion_epub_md": match.group(3) } }
+                    if len(match.group(2) or "") > 2:
+                        sub_entry["parts"] = match.group(2)[1:-1]
+                    sub_entry["refs"] = [{ "book": "twotc", "entry": "otdict", "word": sub_entry["word"] }]
+                    self.add_entry(entries, sub_entry)
                 else:
-                    if entry == None:
-                        match = re.match(r'^\[{0,1}([^\]]+)-*\]{0,1}-*(\([^\)]+\)){0,1}\s*(\S.*)$', line)
-                        if not match:
-                            raise Exception(f"Error: unrecognized entry format: {line}")
-                        entry = { "word": match.group(1), "definition": match.group(3) }
-                        if len(match.group(2) or "") > 2:
-                            entry["part"] = match.group(2)[1:-1]
-                        entry["ref"] = { "book": "twotc", "entry": "otdict", "word": entry["word"] }
-                        entries[entry["word"]] = entry
-                    else:
-                        entry["definition"] += " " + line
-        if entry:
-            self.finish_entry(entries, entry)
+                    sub_entry["definition"]["companion_epub_md"] += " " + line
+        progresser.done()
+        progresser.restart()
+        self.print_n("Checking entries for inline definitions and performing other post-processing", end="", flush=True)
+        unprocessed = entries.copy()
+        for _, entry in unprocessed.items():
+            progresser.tick()
+            print(".", end="", flush=True)
+            for sub_entry in entry:
+                self.finish_sub_entry(entries, sub_entry)
+        for _, entry in entries.items():
+            word = entry[0]["word"]
+            log.debug(f"Final processing for entry: {entry}")
+            for se in entry:
+                d = se["definition"]
+                if d["companion"] == d["companion_epub_md"]:
+                    del d["companion"]
+                if "companion" in d and d["companion"] == d["wotwiki"]:
+                    del d["wotwiki"]
+                if "wotwiki" in d and d["wotwiki"] == d["companion_epub_md"]:
+                    del d["wotwiki"]
+            if len(entry) == 1:
+                entries[word] = entry[0]
+            else:
+                ne = { "word": entry[0]["word"], "entries": entry, "refs": [] }
+                for sub_entry in ne["entries"]:
+                    log.debug(f"Final processing for sub_entry: {sub_entry}")
+                    if sub_entry["word"] != ne["word"]:
+                        raise Exception(f"Error: sub_entry word {sub_entry['word']} does not match entry word {ne['word']}")
+                    del sub_entry["word"]
+                    for ref in sub_entry["refs"]:
+                        ne["refs"].append(ref)
+                    del sub_entry["refs"]
+                if len(ne["refs"]) < 1:
+                    del ne["refs"]
+                entries[word] = ne
+            if word == "Tarmon Gai'don":
+                entries[word]["italicize"] = False
         progresser.done()
         self.print_v(f"Parsed {len(entries)} entries. Writing to {self.parsed_args.out_json}...", end="", flush=True)
         with open(self.parsed_args.out_json, "w") as f:
