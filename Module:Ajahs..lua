f72f59f09125730430a59812c39c2e9c011243b0
Andrew F <ncsuandrew12@gmail.com>
2026-04-10 15:35:59 +0000

    Revert "File extensions (messy)."
    This reverts commit 147d643a77223732f74e6abbc34600c5d0d7174d.

--- Module:Ajahs..lua
+++
@@ -1,9 +0,0 @@
-local m_ajahs_json = mw.loadJsonData('Module:Ajahs/ajahs.json')
-local m = {}
-
-m.emoji_map = {}
-for key, ajah in pairs(m_ajahs_json) do
-	m.emoji_map[string.lower(key)] = ajah.emoji
-end
-
-return m
